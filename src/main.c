19d3a53be2dcd305560c3321b186a44b680e5a8f
gs9pyyffvb-tech <gs9pyyffvb@privaterelay.appleid.com>
2026-09-09 20:59:24 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -14,14 +14,11 @@
 
 typedef NTSTATUS STATUS;
 
-#define STB_IMAGE_IMPLEMENTATION
 #define STBI_NO_STDIO
 #include "stb_image.h"
 
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image_resize.h"
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STBI_WRITE_NO_STDIO
 #include "stb_image_write.h"
 
